919d8ca20492e01796aa77edaf45e131e4d48e99
Adan Reyes <adancatari3@gmail.com>
2022-04-30 03:44:58 -0300

    Create-React-App

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
